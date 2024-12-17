5503ad7c4d826ddd4454daee9c714a92fdc227b5
tacticalDoor3000 <obivan085@gmail.com>
2024-12-17 19:21:58 +0300

    start build

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 import _ from 'lodash';
+import './index.twig'
+import './stylesheet.scss'
 
-import './stylesheet.css'
 
-
-/*
 function importAll(r) {
   return r.keys().map(r);
 }
 
-const images = importAll(require.context('./image', false, /\.(png|jpe?g|svg)$/));
-*/
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+const images = importAll(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,37 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
   mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devServer: {
+    static: './dist',
+    port: 9000,
+    historyApiFallback: true
+  },
   module: {
     rules: [
-
+      {
+        test: /\.txt$/, 
+        use: "raw-loader",
+      },
+      {
+        test: /\.twig$/,
+        use: {
+          loader: 'twig-loader',
+          options: {
+              // See options section below
+              
+          },
+        }
+      },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
@@ -26,9 +46,29 @@ module.exports = {
         test: /\.html$/i,
         loader: 'html-loader',
       },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          // keep original filenames and copy images to `dist/img/`
+          filename: './image/[name][ext]', 
+          //filename: path.join('icons', '[name].[contenthash][ext]'),
+          //filename: 'img/[name].[hash:8][ext][query]',
+        },
+      },
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: 'stylesheet.scss'}),
+    new NodePolyfillPlugin(),
+    new HtmlWebpackPlugin({ 
+      template: './index.twig',
+      //inject: 'body'
+    }),
+    new HtmlWebpackPlugin({
+      filename: './blocks/header.twig',
+      template: './blocks/header.twig',
+      inject: false,
+    }),
+    new MiniCssExtractPlugin({ filename: 'stylesheet.css'}),
   ]
 };
